775401efb73446bccbce005a06487b1c94212085
Bruce Chen <aaaeeettt124@126.com>
2024-09-09 01:25:37 +0800

    adding react route to dependecy

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,15 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login.tsx";
+import Register from "./Register.tsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="flex flex-col p-4 bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">Vite + React</h1>
-      <div className="bg-white p-6 rounded-lg shadow-md">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="px-4 py-2 bg-green-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition-colors"
-        >
-          count is {count}
-        </button>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import Login from "./login.tsx";
-import Register from "./register.tsx";
+import App from "./App";
+
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Login />
-    <Register />
     <App />
   </StrictMode>
 );
