3b5bab278fa66a33c91bf20bdf992576d8b5b152
Bruce Chen <aaaeeettt124@126.com>
2024-09-09 00:31:50 +0800

    change the file sys

--- front/src/login.tsx
+++ front/src/login.tsx
@@ -9,7 +9,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("/api/login", {
+      const response = await fetch("http://localhost:8080/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
